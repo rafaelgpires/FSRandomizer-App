85b2bbd2b41126df02e6dd198920eacfa416f6f0
rafaelgpires <64205524+rafaelgpires@users.noreply.github.com>
2020-04-24 04:07:56 +0100

    Updated Project Settings/Indents
    Renamed cl_readHash.cs as well, next commit has it.

---
+++ FSRandomizer/FSRandomizer/die.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Forms;
+
+namespace FSRandomizer
+{
+    class die
+    {
+        public die(string error)
+        {
+            MessageBox.Show(error, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            if (Application.MessageLoop)
+            {
+                // WinForms app
+                Application.Exit();
+            }
+            else
+            {
+                // Console app
+                System.Environment.Exit(1);
+            }
+        }
+    }
+}
