cd4c80489860f73fe0c1d38294a6c1478362b304
rafaelgpires <64205524+rafaelgpires@users.noreply.github.com>
2020-04-26 16:20:08 +0100

    Added CHLogo to UI

--- FSRandomizer/FSRandomizer/frmMain.Designer.cs
+++ FSRandomizer/FSRandomizer/frmMain.Designer.cs
@@ -30,8 +30,10 @@
         {
 			this.picClose = new System.Windows.Forms.PictureBox();
 			this.picTitleBar = new System.Windows.Forms.PictureBox();
+			this.picCHLogo = new System.Windows.Forms.PictureBox();
 			((System.ComponentModel.ISupportInitialize)(this.picClose)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.picTitleBar)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.picCHLogo)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// picClose
@@ -58,12 +60,22 @@
 			this.picTitleBar.TabIndex = 0;
 			this.picTitleBar.TabStop = false;
 			// 
+			// picCHLogo
+			// 
+			this.picCHLogo.Image = global::FSRandomizer.Properties.Resources.CHLogo;
+			this.picCHLogo.Location = new System.Drawing.Point(46, 47);
+			this.picCHLogo.Name = "picCHLogo";
+			this.picCHLogo.Size = new System.Drawing.Size(207, 137);
+			this.picCHLogo.TabIndex = 2;
+			this.picCHLogo.TabStop = false;
+			// 
 			// frmMain
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(222)))), ((int)(((byte)(222)))));
 			this.ClientSize = new System.Drawing.Size(300, 573);
+			this.Controls.Add(this.picCHLogo);
 			this.Controls.Add(this.picClose);
 			this.Controls.Add(this.picTitleBar);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -74,6 +86,7 @@
 			this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.frmMain_MouseDown);
 			((System.ComponentModel.ISupportInitialize)(this.picClose)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.picTitleBar)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.picCHLogo)).EndInit();
 			this.ResumeLayout(false);
 
         }
@@ -82,6 +95,7 @@
 
 		private System.Windows.Forms.PictureBox picTitleBar;
 		private System.Windows.Forms.PictureBox picClose;
+		private System.Windows.Forms.PictureBox picCHLogo;
 	}
 }
 
